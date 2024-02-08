c94f794202044ea479925d96d7184024b6f2c77e
Vishwajeet Kotkar <vishwajeetak@gmail.com>
2024-02-08 20:11:37 +0530

    Added the Asynchronous configuration file

---
+++ src/main/java/com/emailhandler/email/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package com.emailhandler.email.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "AsyncExecutor")
+    public Executor asyncExecutor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(20);
+        executor.initialize();
+        return executor;
+    }
+}
---
+++ src/main/java/com/emailhandler/email/controller/EmailControllerApi.java
@@ -0,0 +1,2 @@
+package com.emailhandler.email.controller;public class EmailControllerApi {
+}
